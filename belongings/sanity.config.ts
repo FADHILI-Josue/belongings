22354df88c2fc4b4d4385489eddf41dd4bd897d9
FADHILI Josue <FADHILI Josue>
2024-01-26 11:38:57 +0200

    feat: sanity workspaces

--- belongings/sanity.config.ts
+++ belongings/sanity.config.ts
@@ -3,13 +3,15 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 //import {googleMapsInput} from '@sanity/google-maps-input'
 import {schemaTypes} from './schemas'
+import { RiFootballFill } from 'react-icons/ri'
 
-export default defineConfig({
-  name: 'default',
+export default defineConfig([{
+  name: 'belongings',
   title: 'belongings',
-
+  basePath: '/dev',
   projectId: 'qutxk4zb',
   dataset: 'dev',
+  icon: RiFootballFill, 
 
   plugins: [
     deskTool(),
@@ -19,4 +21,21 @@ export default defineConfig({
   schema: {
     types: schemaTypes,
   },
-})
+
+  // tools: [
+  //   {name: 'my-tool', title: 'My Tool', component: (<h1>hello</h1>)},
+  //   {name: 'tool-2', title: '2nd Tool', component: MyOtherTool},
+  // ],
+},
+{
+  projectId: 'qutxk4zb',
+  dataset: 'production',
+  name: 'production',
+  title: 'Production belongings',
+  basePath: '/production',
+  icon: RiFootballFill, 
+  plugins: [deskTool()],
+  schema: {
+    types: schemaTypes,
+  },
+}])
